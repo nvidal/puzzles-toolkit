6ff64d823d365eb3acfe8cc36de1a1378f33842a
esperanzage <esperanzage@gmail.com>
2021-02-13 23:26:56 +1300

    Added styled components

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -4,12 +4,20 @@ declare interface GridProps {
     gridState :{ id: string,  active:boolean}[], 
     onActivateChange: (id: string, value: boolean) => void
 };
+
+
 const Grid = ({ gridState, onActivateChange }: GridProps) => {
 
-    
+    const braileStyles= {
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr", 
+        gridTemplateRows: "1fr 1fr 1fr",
+        width: "auto",
+        alignContent: "start",
+    }
 
     return (
-        <div>
+        <div style={braileStyles}>
                 {gridState.map( el => <GridSymbol id={el.id} onActivateChange={onActivateChange}/> )}
         </div>
 
--- src/components/GridSymbol.tsx
+++ src/components/GridSymbol.tsx
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 declare interface GridSymbolProps { 
     id: string,
-    onActivateChange: (id: string, value: boolean) => void
+    onActivateChange: (id: string, value: boolean) => void,
 };
 const GridSymbol = ({id, onActivateChange}: GridSymbolProps) => {
 
+    const BrailleControl = styled.div `
+        border-radius: 50px;
+        width: 40px;
+        height: 40px;
+        background: ${props => selected ? "linear-gradient(0deg, #1C82FF, #1C82FF)" : "radial-gradient(43.21% 42.05% at 50% 50%, rgba(255, 255, 255, 0.4) 0%, #D4D5DF 100%)"};
+        box-shadow: ${props => selected ? "2px 2px 4px #C0C2CE, -2px -2px 8px #FFFFFF, inset 5px 5px 10px rgba(255, 255, 255, 0.4)" : "inset -5px -5px 16px #FFFFFF"};
+        transition: all ease-in 500ms;
+    `;
+
     const [selected, setSelect] = useState(false);
-    let styles= {
-        backgroundColor: (selected ? "blue" : "pink"),
-        borderRadius: "50px",
-        width: "40px",
-        height: "40px"
-    }
+
     return (
         <div>
-            <div onClick={() => {setSelect(!selected); onActivateChange(id, !selected)}} style={styles} id={id} />
+            <BrailleControl onClick={() => {setSelect(!selected); onActivateChange(id, !selected)}} id={id} />
         </div>
 
     )
